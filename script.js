8d1447ac0570b324106363a89980dee574cfbdef
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-04 10:07:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,149 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqg-Be1DC9O6SH5rXGbMjDds0wG9rolRc",
+    authDomain: "galinho-fbdb2.firebaseapp.com",
+    projectId: "galinho-fbdb2",
+    storageBucket: "galinho-fbdb2.firebasestorage.app",
+    messagingSenderId: "1088809563797",
+    appId: "1:1088809563797:web:7c250b6913198f1f700d9c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+let saldo = 0;
+let din_apostado = 0;
+let currentUID = null;
+let bloqueado = false;
+
+async function criarUsuarioDB(uid) {
+    await setDoc(doc(db, "users", uid), {
+        saldo: 0,
+        din_apostado: 0,
+        rodadaGratisUsada: false
+    });
+}
+
+async function carregarDados(uid) {
+    const snap = await getDoc(doc(db, "users", uid));
+    return snap.exists() ? snap.data() : null;
+}
+
+async function salvarDados() {
+    if (!currentUID) {
+        console.warn("Tentativa de salvar sem UID definido!");
+        return;
+    }
+
+    try {
+        await updateDoc(doc(db, "users", currentUID), {
+            saldo,
+            din_apostado
+        });
+        console.log("Dados salvos:", { saldo, din_apostado });
+    } catch (err) {
+        console.error("Erro ao salvar no Firestore:", err);
+    }
+}
+
+
+async function fazerCadastro(email, senha) {
+    const cred = await createUserWithEmailAndPassword(auth, email, senha);
+    await criarUsuarioDB(cred.user.uid);
+    alert("Usuário criado!");
+    currentUID = cred.user.uid;
+    logando();
+    guilhermeLogou(cred.user);
+}
+
+async function fazerLogin(email, senha) {
+    const cred = await signInWithEmailAndPassword(auth, email, senha);
+    currentUID = cred.user.uid;
+    
+    if (guilhermeLogou(cred.user)) {
+        return;
+    }
+    const dados = await carregarDados(currentUID);
+
+    saldo = dados.saldo;
+    din_apostado = dados.din_apostado;
+
+    atualizarTela();
+    logando()
+}
+
+
+function atualizarTela() {
+    document.getElementById("saldo").textContent =
+        "Saldo: R$" + saldo.toFixed(2);
+
+    document.getElementById("din-apostado").textContent =
+        "Dinheiro apostado: R$" + din_apostado.toFixed(2);
+
+    salvarDados();
+}
+
+
+document.getElementById("fazerCadastro")
+.addEventListener("click", async () => {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senhaRegistro").value;
+    await fazerCadastro(email, senha);
+});
+
+document.getElementById("fazerLogin")
+.addEventListener("click", async () => {
+    const email = document.getElementById("usuario").value;
+    const senha = document.getElementById("senhaLogin").value;
+    await fazerLogin(email, senha);
+});
+
+
+
 //? DIVS
-const menu = document.getElementById("menu");
-const jogo = document.getElementById("jogo");
-const aposta1 = document.getElementById("aposta1");
-const aposta2 = document.getElementById("aposta2");
-const aposta3 = document.getElementById("aposta3");
-const banner = document.getElementById("ad");
-const divapostado = document.getElementById("din-apostado");
-const divsaldo = document.getElementById("saldo");
+const menu             = document.getElementById("menu");
+const jogo             = document.getElementById("jogo");
+const aposta1          = document.getElementById("aposta1");
+const aposta2          = document.getElementById("aposta2");
+const aposta3          = document.getElementById("aposta3");
+const banner           = document.getElementById("ad");
 const apostar_dinheiro = document.getElementById("apostar-dinheiro");
-const popup_erro = document.getElementById("popup_erro");
-const mensagem_erro = document.getElementById("mensagem-erro");
-const popup_qualquer = document.querySelector(".popup");
-const popup_perdeu = document.getElementById("popup_perdeu");
-const popup_ganhou = document.getElementById("popup_ganhou");
+const popup_erro       = document.getElementById("popup_erro");
+const mensagem_erro    = document.getElementById("mensagem-erro");
+const popup_perdeu     = document.getElementById("popup_perdeu");
+const popup_ganhou     = document.getElementById("popup_ganhou");
+const DivLogin         = document.getElementById("login");
+const DivRegistro      = document.getElementById("registro");
+const popup_gratis     = document.getElementById("popup_gratis");
+
+//? LISTENER
+document.querySelector(".botao").addEventListener("click", jogar);
+document.querySelectorAll(".botaovolta").forEach(btn => {
+    btn.addEventListener("click", voltar);
+});
+document.querySelector(".botaoConta").addEventListener("click", abrirLogin);
+document.querySelector(".botaoReg").addEventListener("click", abrirRegistro);
+document.getElementById("bt-dobrar-aposta").addEventListener("click", fdobrar_aposta);
+document.querySelector(".botaogirar").addEventListener("click", girar);
+document.getElementById("bt-apostar").addEventListener("click", fapostar);
+document.querySelectorAll(".botaovolta2").forEach(btn => {
+    btn.addEventListener("click", fechapopup);
+});
+document.getElementById("bt-ok").addEventListener("click", fapostarok);
+document.getElementById("rodada_gratis").addEventListener("click", rodadagratis)
 
 //? COISAS
 const botaogirar = document.querySelector(".botaogirar");
-let saldo = 0;
-let din_apostado = 0;
+
 
 //? FUNCOES
 
@@ -26,26 +152,40 @@ function jogar(){
     aposta2.innerHTML = "";
     aposta3.innerHTML = "";
     
-    menu.style.display = "none";
-    jogo.style.display = "flex";  
+    menu.style.display     = "none";
+    jogo.style.display     = "flex";  
+    DivLogin.style.display = "none";
 };
 
 function voltar(){
-    menu.style.display = "flex";
-    jogo.style.display = "none";  
+    menu.style.display        = "flex";
+    jogo.style.display        = "none";  
+    DivLogin.style.display    = "none";
+    DivRegistro.style.display = "none";
 };
 
-function attTela(){
-    divapostado.textContent = "Dinheiro apostado: R$" + din_apostado.toFixed(2);
-    divsaldo.textContent = "Saldo: R$" + saldo.toFixed(2);    
+function abrirLogin(){
+    DivLogin.style.display = "flex";
+    menu.style.display     = "none";
 };
-attTela();
+
+function abrirRegistro(){
+    DivLogin.style.display    = "none";
+    DivRegistro.style.display = "flex";
+}
+
+function logando(){
+    DivLogin.style.display = "none";
+    DivRegistro.style.display = "none";
+    jogar();
+}
 
 const imgs_ad = [
-    "imgs/restaurante.png",
-    "imgs/subwaymoney.jpeg",
-    "imgs/galinhoad.png"
+    "/imgs/restaurante.png",
+    "/imgs/subwaymoney.jpeg",
+    "/imgs/galinhoad.png"
 ]
+
 let pos = 0;
 const imgElement = document.createElement("img");
 imgElement.src = imgs_ad[pos];
@@ -59,6 +199,13 @@ let intervalo = setInterval(() => {
 
 
 function girar(){
+
+    if (bloqueado) {
+        mensagem_erro.textContent = "galinho não quer que voce aposte, sai daqui guilherme";
+        popup_erro.style.display = "flex";
+        return;
+    }
+
     if (din_apostado >= 10){
     botaogirar.style.visibility = "hidden";
 
@@ -105,7 +252,7 @@ function girar(){
                 saldo += din_apostado * 1.5 - din_apostado;
             }
             popup_ganhou.style.display = "flex";
-            attTela();
+            atualizarTela();
             setTimeout( () => {
                 popup_ganhou.style.display = "none";
             }, 2000)
@@ -115,7 +262,7 @@ function girar(){
             console.log('perdeu kkk');
             popup_perdeu.style.display = "flex";
             din_apostado = din_apostado * 0.4;
-            attTela();
+            atualizarTela();
             setTimeout( () => {
                 popup_perdeu.style.display = "none";
             }, 2000)
@@ -123,7 +270,7 @@ function girar(){
        }, 2000);
     }
     else {
-        mensagem_erro.innerHTML = "pobre sem dinheiro kkk <br> mínimo pra apostar: R$10";
+        mensagem_erro.innerHTML = "pobre sem dinheiro kkk </br> mínimo pra apostar: R$10";
         popup_erro.style.display = "flex";
         apostar_dinheiro.style.display = "none";
     }
@@ -149,7 +296,7 @@ function aleatorio(){
         "porco.png"
     ];
 
-    img.src = "imgs/" + imagens[num];
+    img.src = "/imgs/" + imagens[num];
     img.dataset.tipo = imagens[num]; 
     return img;
 };
@@ -160,17 +307,22 @@ function fapostar(){
 }
 
 function fapostarok(){
+
+    if (bloqueado) {
+        mensagem_erro.textContent = "nem tenta guilherme KKKKK";
+        popup_erro.style.display = "flex";
+        return;
+    }
+
     const input = document.getElementById("quantia-apostar");
     const quantia_apostar = parseFloat(input.value);
 
     if (quantia_apostar > 0) {
         din_apostado += quantia_apostar;
-        attTela();
+        atualizarTela();
         input.value = "";
         fechapopup();
     }
-    
-    
 };
 
 function fdobrar_aposta(){
@@ -178,7 +330,7 @@ function fdobrar_aposta(){
     if (saldo >= 2 * din_apostado && saldo > 0){
         saldo -= din_apostado;
         din_apostado = 2 * din_apostado;
-        attTela();
+        atualizarTela();
     } else {
         mensagem_erro.textContent = "Dinheiro insuficiente"
         popup_erro.style.display = "flex";
@@ -189,4 +341,47 @@ function fdobrar_aposta(){
 function fechapopup(){
     apostar_dinheiro.style.display = "none";
     popup_erro.style.display = "none";
+}
+
+function guilhermeLogou(user) {
+    if (user.email.toLowerCase().startsWith("gui")){
+        saldo = 0;
+        din_apostado = 0;
+
+        bloqueado = true; 
+
+        atualizarTela();
+
+        mensagem_erro.textContent = "voce nao pode apostar guilherme :/ sai daqui e vai trabalhar";
+        popup_erro.style.display = "flex";
+
+        return true;
+    }
+    return false;
+}
+
+async function rodadagratis(){
+    if (!currentUID) return;
+
+    const dados = await carregarDados(currentUID);
+
+    if (dados.rodadaGratisUsada) {
+        mensagem_erro.textContent = "ja ganhou a rodada gratis idiota aproveitador quem vc acha q a gente eh pra da dinheiro assim de graca";
+        popup_erro.style.display = "flex";
+        return;
+    }
+
+    saldo += 10;
+
+    await updateDoc(doc(db, "users", currentUID), {
+        rodadaGratisUsada: true,
+        saldo: saldo  
+    });
+
+    popup_gratis.style.display = "flex";
+    atualizarTela();
+
+    setTimeout(() => {
+        popup_gratis.style.display = "none";
+    }, 2000);
 }
